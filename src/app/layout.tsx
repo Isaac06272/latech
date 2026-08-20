a82ba552a65cf4850cd0048cd69ca403ef1adf75
Isaac06272 <johngwenisaacaustria@gmail.com>
2026-08-20 18:55:53 +0800

    logo fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'LaTech Dashboard',
   description: 'Your intelligence digest for the week',
+  icons: {
+    icon: 'https://lh3.googleusercontent.com/aida-public/AB6AXuCnJxjxRvu1UbNiCsPBXNIhaotWwMuqebhF902SAzlEqjfqjipTSW0ZWN55S3nNnwrjs2YbN4xoFUopQ92WRL3WnqZIQ_sunzN5mCmkGbjVmphW0iykhTtFwezuSltAGgHEl3grA1dTp5SgQkAgg5IV66GLUNNk60Ic8xM-ED5LP48J_6OR_-rlDIipXuBiUicAc8Rl6_ai5ZXFIZxjH3h_YBvdVUBulQntvhY1u_vcMD7hcLcGGjgugw',
+    shortcut: 'https://lh3.googleusercontent.com/aida-public/AB6AXuCnJxjxRvu1UbNiCsPBXNIhaotWwMuqebhF902SAzlEqjfqjipTSW0ZWN55S3nNnwrjs2YbN4xoFUopQ92WRL3WnqZIQ_sunzN5mCmkGbjVmphW0iykhTtFwezuSltAGgHEl3grA1dTp5SgQkAgg5IV66GLUNNk60Ic8xM-ED5LP48J_6OR_-rlDIipXuBiUicAc8Rl6_ai5ZXFIZxjH3h_YBvdVUBulQntvhY1u_vcMD7hcLcGGjgugw',
+    apple: 'https://lh3.googleusercontent.com/aida-public/AB6AXuCnJxjxRvu1UbNiCsPBXNIhaotWwMuqebhF902SAzlEqjfqjipTSW0ZWN55S3nNnwrjs2YbN4xoFUopQ92WRL3WnqZIQ_sunzN5mCmkGbjVmphW0iykhTtFwezuSltAGgHEl3grA1dTp5SgQkAgg5IV66GLUNNk60Ic8xM-ED5LP48J_6OR_-rlDIipXuBiUicAc8Rl6_ai5ZXFIZxjH3h_YBvdVUBulQntvhY1u_vcMD7hcLcGGjgugw',
+  },
 }
 
 export default function RootLayout({
